45fb06de1066a986283684756361974bce00a60c
Rafael Marques <rafaelmarques.rm86@gmail.com>
2022-10-27 11:52:23 -0300

    Recapitulação

---
+++ Recapitulacao 2 exec 2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar. */
+
+int numero;
+
+Console.WriteLine("Informe um numero:");
+numero = int.Parse(Console.ReadLine());
+
+if (numero %2 == 0)
+{
+    Console.WriteLine("Par");
+}else
+{
+    Console.WriteLine("Impar");
+}
